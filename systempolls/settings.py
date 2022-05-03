9aee570d201feeb2b72f558a00f780ba676e32f4
iElectro <ejose.acc@gmail.com>
2022-05-02 19:33:56 -0500

    basedir

--- systempolls/settings.py
+++ systempolls/settings.py
@@ -16,7 +16,7 @@ from dotenv import load_dotenv
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 load_dotenv()
 
